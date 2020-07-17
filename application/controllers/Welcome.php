9bdabd30d50de78f288faace682ba50c0b8a973e
elihe999 <eli.he999@hotmail.com>
2020-07-17 17:18:34 +0800

    <chore> Add swagger

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,25 +1,34 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+require APPPATH.'/libraries/RestController.php';
+require APPPATH.'/libraries/Format.php';
 
-class Welcome extends CI_Controller {
+use chriskacerguis\RestServer\RestController;
+
+class Welcome extends RestController {
+
+	function __construct()
+    {
+        // Construct the parent class
+        parent::__construct();
+	}
+
 
 	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 * @OA\Info(title="mysq", version="0.1")
+	 * 
 	 */
-	public function index()
+
+	
+	/**
+      * @OA\Get(
+	  *	  path="/api/resource.json",
+	  *	  @OA\Response(response="200", description="1")
+	  * )
+      * 
+      */
+	public function setting_get()
 	{
-		$this->load->view('welcome_message');
+		
 	}
 }
